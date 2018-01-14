e23c6001fd82d34758ad7d047f1ed53a2e3a0f76
ynosov <34068554+ynosov@users.noreply.github.com>
2018-01-14 18:03:44 +0200

    Выделен класс для управления сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -6,32 +7,15 @@ class Application:
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://addressbook/group.php")
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_id("content").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_id("content").click()
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
     def open_groups_page(self):
         wd = self.wd
         wd.find_element_by_css_selector("body").click()
-
-    def logout(self):
-        wd = self.wd
-        # logout
-        wd.find_element_by_link_text("Logout").click()
 
     def return_to_groups_page(self):
         wd = self.wd
